329cfdfaea182c9b16039412ea6b11803ef93a9b
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-17 09:44:22 +0100

    applicazione terminata

--- LancioDadi/MainWindow.xaml.cs
+++ LancioDadi/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,43 @@ namespace LancioDadi
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random r = new Random();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Btn_tira_Click(object sender, RoutedEventArgs e)
+        {
+            int d1 = 0;
+            int d2 = 0;
+            
+
+            var Dado1 = new Thread((currentIndex) =>
+            {
+                
+                d1 = r.Next(1, 7);
+            });
+
+            var Dado2 = new Thread((currentIndex) =>
+            {
+                
+                d2 = r.Next(1, 7);
+            });
+
+            Dado1.Start();
+            Dado2.Start();
+
+            Dado1.Join(200);
+            Dado2.Join(400);
+
+            int Tiro = d1 + d2;
+
+            lbl_tira.Content = Tiro;
+
+            img_dado1.Source = new BitmapImage(new Uri($"{d1}.png", UriKind.Relative));
+            img_dado2.Source = new BitmapImage(new Uri($"{d2}.png", UriKind.Relative));
+
         }
     }
 }
